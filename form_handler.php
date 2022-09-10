db8fee2a9f0fd64248595cb3b0c2da359e5d441c
Abhishek Singh <91690267+isinghabhishek@users.noreply.github.com>
2022-09-11 00:23:33 +0530

    Add files via upload

---
+++ form_handler.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'info@yourwebsite.com';
+
+$email_subject = 'New Form Submission';
+
+$email_boby = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message .\n";
+
+
+$to = 'singhabhishek3006.abhi@gmail.com';
+
+$headers = "From: $email_form \r\n";
+
+$headers .= "Reply-To: $Visitor_email \r\n";
+
+mail($to,$email_subject,$email_boby,$headers);
+
+header("Location: contact.html");
+?>
